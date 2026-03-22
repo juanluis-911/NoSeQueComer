252e83f6286b2d95d751ecaeadec0a6275429fa0
Juan Luis Ruiz <juanluis.ruiz@dm.solar>
2026-03-22 14:04:56 -0600

    add analitycs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import InstallPWA from '@/components/InstallPWA'
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -48,6 +49,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${inter.className} antialiased bg-[#FAFAFA]`}>
         {children}
         <InstallPWA />
+        <Analytics />
       </body>
     </html>
   )
